f0061e31dee0158341594cc2766fb49d0571bf5e
virangaj <virangapasindu4@gmail.com>
2023-04-22 23:11:33 +0530

    service section done

--- src/pages/Home/ServiceSection.tsx
+++ src/pages/Home/ServiceSection.tsx
@@ -2,10 +2,38 @@ import React from 'react';
 import HeadingText from '../../components/shared/HeadingText';
 import ServiceCard from './shared/ServiceCard';
 
+const data = [
+	{
+		title: 'Remote Team Building',
+		img: 'https://m.economictimes.com/thumb/msid-95779647,width-2123,height-1413,resizemode-4,imgsize-238776/digital-transformation-istock.jpg',
+		desc: '',
+	},
+	{
+		title: 'Project management',
+		img: 'https://m.economictimes.com/thumb/msid-95779647,width-2123,height-1413,resizemode-4,imgsize-238776/digital-transformation-istock.jpg',
+		desc: '',
+	},
+	{
+		title: 'Product development',
+		img: 'https://m.economictimes.com/thumb/msid-95779647,width-2123,height-1413,resizemode-4,imgsize-238776/digital-transformation-istock.jpg',
+		desc: '',
+	},
+	{
+		title: 'RND',
+		img: 'https://m.economictimes.com/thumb/msid-95779647,width-2123,height-1413,resizemode-4,imgsize-238776/digital-transformation-istock.jpg',
+		desc: '',
+	},
+	{
+		title: 'Business Operations',
+		img: 'https://m.economictimes.com/thumb/msid-95779647,width-2123,height-1413,resizemode-4,imgsize-238776/digital-transformation-istock.jpg',
+		desc: '',
+	},
+];
+
 function ServiceSection() {
 	return (
 		<>
-			<div className='flex-area'>
+			<div className='mb-10 flex-area'>
 				<HeadingText title='about us' />
 				<p className='w-[40%] gray-text'>
 					We have advanced skills & resources to create large-scale solutions as
@@ -13,7 +41,9 @@ function ServiceSection() {
 				</p>
 			</div>
 
-			<ServiceCard />
+			{data.map((d: any, i: number) => (
+				<ServiceCard {...d} key={i} />
+			))}
 		</>
 	);
 }
--- src/pages/Home/shared/ServiceCard.tsx
+++ src/pages/Home/shared/ServiceCard.tsx
@@ -3,10 +3,13 @@ import { RxDotFilled } from 'react-icons/rx';
 import { FiArrowUpRight, FiArrowDownRight } from 'react-icons/fi';
 import OutlineButton from '../../../components/shared/OutlineButton';
 
-function ServiceCard() {
-	const [active, setActive] = useState(false);
+function ServiceCard({ title, img, desc }: any) {
+	const [active, setActive] = useState<boolean>(false);
 
-	console.log(active);
+	const detailsSection = () => {
+		setActive(!active);
+		// console.log(active);
+	};
 	return (
 		<>
 			<div
@@ -14,37 +17,36 @@ function ServiceCard() {
 					active ? 'service-card-container-active' : 'service-card-container'
 				}`}
 			>
-				<div
-					className={`flex items-center justify-between  h-40  w-[93%] mx-auto ${
-						active ? 'cursor-default' : 'cursor-pointer'
-					}`}
-					onClick={() => setActive((val) => !val)}
-				>
-					<p className='flex items-center'>
-						<RxDotFilled
-							className={`w-10 h-10 mr-10 ${
-								active ? 'text-black' : 'text-primary'
-							}`}
-						/>
-						<span className='service-txt-head'>Remote Team Building</span>
-					</p>
-					{active ? (
-						// when drop down ois active
-						<FiArrowUpRight
-							className={`w-16 h-16 mr-10 cursor-pointer z-30`}
-							onClick={() => setActive((val) => !val)}
-						/>
-					) : (
-						<FiArrowDownRight className={`w-16 h-16 mr-10 cursor-pointer`} />
-					)}
+				<div className={`${active ? '' : 'service-card-animation'}`}>
+					<div
+						className={`flex items-center justify-between w-[97%] h-40  mx-auto ${
+							active ? 'cursor-default' : 'cursor-pointer'
+						}`}
+						onClick={detailsSection}
+					>
+						<p className='flex items-center'>
+							<RxDotFilled
+								className={`w-10 h-10 mr-10 ${
+									active ? 'text-black' : 'text-primary dot-color'
+								}`}
+							/>
+							<span className='service-txt-head'>{title}</span>
+						</p>
+						{active ? (
+							// when drop down ois active
+							<FiArrowUpRight
+								className={`w-16 h-16 mr-10 cursor-pointer !z-50`}
+								onClick={detailsSection}
+							/>
+						) : (
+							<FiArrowDownRight className={`w-16 h-16 mr-10 cursor-pointer`} />
+						)}
+					</div>
 				</div>
 
 				{active ? (
-					<div className='service-detail-section'>
-						<img
-							src='https://m.economictimes.com/thumb/msid-95779647,width-2123,height-1413,resizemode-4,imgsize-238776/digital-transformation-istock.jpg'
-							className='w-[50%] rounded-md mr-10'
-						/>
+					<div className='relative service-detail-section'>
+						<img src={img} className='w-[50%] rounded-md mr-10' />
 						<div className='w-[50%]'>
 							<p className='para-text'>
 								Lorem ipsum dolor sit amet consectetur adipisicing elit. At
@@ -52,7 +54,9 @@ function ServiceCard() {
 								debitis minima unde illo itaque, rerum enim placeat maxime vel
 								animi. Provident, in?
 							</p>
-							<OutlineButton title='Learn More' />
+							<div className='absolute bottom-0'>
+								<OutlineButton title='Learn More' />
+							</div>
 						</div>
 					</div>
 				) : (
